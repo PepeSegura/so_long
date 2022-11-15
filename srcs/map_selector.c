4606fdd830fc4a66c70c787ff7f865e14a832355
Pepe Segura Carreno <psegura-@c1r11s4.42madrid.com>
2022-11-15 21:59:09 +0100

    More maps to the selector

--- srcs/map_selector.c
+++ srcs/map_selector.c
@@ -6,7 +6,7 @@
 /*   By: psegura- <psegura-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 15:41:42 by psegura-          #+#    #+#             */
-/*   Updated: 2022/11/15 17:50:00 by psegura-         ###   ########.fr       */
+/*   Updated: 2022/11/15 21:57:48 by psegura-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,19 @@ void	printf_selector(void)
 	ft_printf("\033c");
 	ft_printf("\033c\033[0;94mChoose a map:\n\n");
 	ft_printf("\033[0;96m1 -> subject_small_map\n");
-	ft_printf("\033[0;96m2 -> subject_big_map\n");
-	ft_printf("\033[0;96m3 -> map_with_enemies\n");
-	ft_printf("\033[0;96m4 -> psegura-\n");
+	ft_printf("\033[0;96m2 -> map_with_enemies\n");
+	ft_printf("\033[0;96m3 -> subject_big_map\n");
+	ft_printf("\033[0;96m4 -> min_horizontal\n");
+	ft_printf("\033[0;96m5 -> min_vertical\n");
+	ft_printf("\033[0;96m6 -> laberinth\n");
+	ft_printf("\033[0;96m7 -> psegura-\n");
 	ft_printf("\033[0;94;5m\nWrite a number between 1-7 and press enter:\n\n");
+}
+
+char	*ft_free_map(char *file, char *path)
+{
+	free(file);
+	return (path);
 }
 
 char	*ft_map_selector(char *file)
@@ -28,25 +37,19 @@ char	*ft_map_selector(char *file)
 	printf_selector();
 	file = get_next_line(0);
 	if (file[0] == '1' && file[1] == '\n')
-	{
-		free(file);
-		return ("maps/valid_maps/subject_map_1.ber");
-	}
+		return (ft_free_map(file, "maps/valid_maps/subject_map_1.ber"));
 	if (file[0] == '2' && file[1] == '\n')
-	{
-		free(file);
-		return ("maps/valid_maps/subject_map_2.ber");
-	}
+		return (ft_free_map(file, "maps/valid_maps/subject_map_enemies.ber"));
 	if (file[0] == '3' && file[1] == '\n')
-	{
-		free(file);
-		return ("maps/valid_maps/subject_map_enemies.ber");
-	}
+		return (ft_free_map(file, "maps/valid_maps/subject_map_2.ber"));
 	if (file[0] == '4' && file[1] == '\n')
-	{
-		free(file);
-		return ("maps/valid_maps/pepe.ber");
-	}
+		return (ft_free_map(file, "maps/valid_maps/min_horizontal_map.ber"));
+	if (file[0] == '5' && file[1] == '\n')
+		return (ft_free_map(file, "maps/valid_maps/min_vertical_map.ber"));
+	if (file[0] == '6' && file[1] == '\n')
+		return (ft_free_map(file, "maps/valid_maps/laberinth.ber"));
+	if (file[0] == '7' && file[1] == '\n')
+		return (ft_free_map(file, "maps/valid_maps/pepe.ber"));
 	free(file);
 	ft_print_error(DIDNT_CHOOSED, "You didn't choose a valid number");
 	return (NULL);
